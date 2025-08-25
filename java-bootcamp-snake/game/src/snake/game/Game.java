092d3b901b041c995795858b29e00fb235f74727
iLorenzz <ldutra0912@gmail.com>
2025-08-25 01:44:45 -0300

    fix: add logger to game class and remove from launcher class

--- java-bootcamp-snake/game/src/snake/game/Game.java
+++ java-bootcamp-snake/game/src/snake/game/Game.java
@@ -1,10 +1,14 @@
 package snake.game;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Game {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Game.class);
+
     public void start(){
         System.out.println("Starting");
+        LOGGER.debug("Teste");
     }
 }
--- java-bootcamp-snake/game/src/snake/game/Launcher.java
+++ java-bootcamp-snake/game/src/snake/game/Launcher.java
@@ -1,10 +1,6 @@
 package snake.game;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Launcher {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Game.class);
 
     public static void main(String[] args) {
         new Game().start();
